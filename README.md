3f7dc7e4aad61ec317fa8835590d51d72611e62e
tratteo <bltmtt@gmail.com>
2025-07-17 20:29:56 +0200

    Init

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ You can use this template as a starting point for any of your projects. Enjoy!
 ## Tutorials and examples
 This repository is going to have different branches. Each branch is relative to a specific tutorial I posted on my channel.
 
-✅ [Nuxt + Leaflet native integration](https://youtu.be/B24nLqwWzKI)
+### ✅ [Nuxt + Leaflet native integration](https://youtu.be/B24nLqwWzKI)  
+-  Branch: [`leaflet-native-integration`](https://github.com/tratteo/nuxt_template/tree/leaflet-native-integration)
 
 ## ⚡Getting started
 Install dependencies
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,28 +1,7 @@
 <template>
-    <div class="w-full flex flex-col gap-4 items-center mt-4">
-        <ClientOnly>
-            <MapView map-id="default-id" :height="'50svh'" width="50%" :markers="markers"></MapView>
-        </ClientOnly>
-    </div>
+    <div class="w-full flex flex-col gap-4 items-center mt-4">Hello!</div>
 </template>
 
-<script lang="ts" setup>
-import type { MarkerDefinition } from "~/components/map_view.vue";
-import { defaultMarkers } from "~/src/markers";
-
-const markers = computed(() => {
-    return defaultMarkers.map(
-        (m) =>
-            <MarkerDefinition>{
-                id: m.id,
-                coords: m.coords,
-                popup: `<p class="text-center" onclick="alert('Ugly dangerous HTML executed!');">${m.id}</p>`,
-                onClick(ev) {
-                    console.log(ev.target);
-                },
-            }
-    );
-});
-</script>
+<script lang="ts" setup></script>
 
 <style></style>
