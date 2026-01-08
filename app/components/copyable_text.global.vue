ca64ace8e95ab9de737052e48ea4008d71181e62
tratteo <bltmtt@gmail.com>
2026-01-08 16:47:47 +0100

    Dependencies

--- app/components/copyable_text.global.vue
+++ app/components/copyable_text.global.vue
@@ -1,27 +1,27 @@
 <template>
-    <u-field-group class="w-full">
-        <u-input :model-value="content" aria-label="Copy text" readonly class="w-full"> </u-input>
+    <u-field-group class="w-full" :size="size">
+        <u-input :model-value="content" aria-label="Copy text" readonly class="w-full" :size="size"> </u-input>
         <u-button
             @click="onClick"
             type="button"
             variant="subtle"
             :color="copied ? 'success' : 'neutral'"
-            size="sm"
+            :size="size"
             aria-label="copy content"
             :icon="copied ? 'material-symbols:check-circle-rounded' : 'material-symbols:content-copy-outline-rounded'"
         ></u-button>
-        <u-dropdown-menu v-if="(items?.length ?? 0) > 0" :items="props.items ?? []">
-            <u-button color="neutral" variant="subtle" icon="i-lucide-chevron-down" size="sm" />
+        <u-dropdown-menu v-if="(items?.length ?? 0) > 0" :items="props.items ?? []" :size="size">
+            <u-button color="neutral" variant="subtle" icon="i-lucide-chevron-down" :size="size" />
         </u-dropdown-menu>
     </u-field-group>
 </template>
 
 <script lang="ts" setup>
-import type { DropdownMenuItem } from "@nuxt/ui";
+import type { ButtonProps, DropdownMenuItem } from "@nuxt/ui";
 
 const clipboard = useClipboard();
 const copied = ref<boolean>(false);
-const props = defineProps<{ content: string; items?: DropdownMenuItem[] }>();
+const props = defineProps<{ content: string; items?: DropdownMenuItem[]; size?: ButtonProps["size"] }>();
 const emit = defineEmits<{ (e: "click", val: string, ev: MouseEvent): void }>();
 
 function onClick(event: MouseEvent) {
